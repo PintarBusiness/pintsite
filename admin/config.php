55cf72d55b1ac00cbc10bdff08009e994ab8a1dc
PintarBusiness <160147958+PintarBusiness@users.noreply.github.com>
2026-05-07 20:35:52 +0200

    prenovljen config

--- admin/config.php
+++ admin/config.php
@@ -3,12 +3,7 @@ session_start();
 
 define("ADMIN_USER", "info@pintsite.si");
 
-/*
-GENERIRAJ HASH:
-<?php echo password_hash("TVOJE_GESLO", PASSWORD_DEFAULT); ?>
-*/
-
-define("ADMIN_PASS", password_hash("1234", PASSWORD_DEFAULT));
+define("ADMIN_PASS", "$2y$10$1jfqtvXqd5k3FmmL6OYXb.7QfLZpGsbSv8AXVMorYTCEyb4PPuzXO");
 
 define("MAIL_FROM", "info@pintsite.si");
 ?>
